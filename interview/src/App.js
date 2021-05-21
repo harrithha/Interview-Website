dfafb44f4663ec74f81840c9331bb0fc421ed607
Your Name <harrithha64@gmail.com>
2021-05-21 09:40:59 +0530

    NavBar Basic

--- interview/src/App.js
+++ interview/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import * as ReactBootStrap from 'react-bootstrap'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Interview website</h1>
+<Navbar/>
     </div>
   );
 }
