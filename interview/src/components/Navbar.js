064d4d3eeca0e63f38ffdca33b9bfee12c9a4933
siddahwala02 <siddharthwala02@gmail.com>
2021-05-23 10:17:12 +0530

    navbar changes

--- interview/src/components/Navbar.js
+++ interview/src/components/Navbar.js
@@ -7,22 +7,19 @@ function Navbar() {
         <div class='sticky-top' >
             
     <ReactBootStrap.Navbar className="nav" collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <ReactBootStrap.Navbar.Brand className="navbrand" href="#home">Company Login</ReactBootStrap.Navbar.Brand>
+  <ReactBootStrap.Navbar.Brand className="navbrand" href="#home">SyneView</ReactBootStrap.Navbar.Brand>
   <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-    <ReactBootStrap.Nav className="mr-auto">
+    <ReactBootStrap.Nav className="ml-auto">
       <ReactBootStrap.Nav.Link className="navitem" href="#features">Features</ReactBootStrap.Nav.Link>
       <ReactBootStrap.Nav.Link className="navitem" href="#pricing">Pricing</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+      <ReactBootStrap.NavDropdown className="navitem" title="Dropdown" id="collasible-nav-dropdown">
         <ReactBootStrap.NavDropdown.Item href="#action/3.1">Action</ReactBootStrap.NavDropdown.Item>
         <ReactBootStrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootStrap.NavDropdown.Item>
         <ReactBootStrap.NavDropdown.Item href="#action/3.3">Something</ReactBootStrap.NavDropdown.Item>
         <ReactBootStrap.NavDropdown.Divider />
         <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item>
       </ReactBootStrap.NavDropdown>
-    </ReactBootStrap.Nav>
-    <ReactBootStrap.Nav>
-      <ReactBootStrap.Navbar.Brand href="#home">Interviwee Login</ReactBootStrap.Navbar.Brand>
     </ReactBootStrap.Nav>
   </ReactBootStrap.Navbar.Collapse>
 </ReactBootStrap.Navbar>
